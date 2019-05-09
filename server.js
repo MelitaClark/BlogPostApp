4a58d252919e88330e0f0059a0abfea90cd5dd57
Melita Clark <beyonce@thinkful.comgit config --global user.email lanis2boys@gmail.com>
2019-05-09 11:03:43 -0700

    new commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {ShoppingList, Recipes} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+
+// log the http layer
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
